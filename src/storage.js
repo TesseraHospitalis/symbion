660ee05832ec726d0d658dd04c70c26058a505af
TesseraHospitalis <dharford2014@gmail.com>
2026-04-06 00:23:40 -0700

    Update Supabase key

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://bdnojpohkheurcaacmtp.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkbm9qcG9ra2hldXJjYWFjbXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMTM0NzksImV4cCI6MjA1NjY4OTQ3OX0.GBmc5_7uncNz61GuDA8SMINqzUipyMB1hvDF_wWIdtA'
+const SUPABASE_ANON_KEY = 'sb_publishable_ZabAZN2jgy0cDVyvzyKRGQ_PveCskOc'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
